ea3c4a7c3f5d21e985630ee2a200a1b9add26a9f
john keru <keruk355@gmail.com>
2024-05-21 20:48:29 +0800

    landing page looks awesome

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -94,6 +94,7 @@ const LandingPage = () => {
         <div>
             <h1>
                 Hahaha sorry pero dinown ko muna server dahil maling card nagamit namin for online billing! 😂
+                Pero di rin to tatagal makikita nyo yung napagandang content neto 😉.
                 <br />
                 -Keru
             </h1>
